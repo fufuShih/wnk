1af6fbe45424ea58ccf20adc57518e44e46485e1
Felix Shih <zx8714245@gmail.com>
2025-12-13 04:05:50 +0800

    feat: init project

---
+++ build.zig
@@ -0,0 +1,107 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const dvui_dep = b.dependency("dvui", .{ .target = target, .optimize = optimize, .backend = .sdl3 });
+
+    const mod = b.addModule("wnk", .{
+        // The root source file is the "entry point" of this module. Users of
+        // this module will only be able to access public declarations contained
+        // in this file, which means that if you have declarations that you
+        // intend to expose to consumers that were defined in other files part
+        // of this module, you will have to make sure to re-export them from
+        // the root file.
+        .root_source_file = b.path("src/root.zig"),
+        // Later on we'll use this module as the root module of a test executable
+        // which requires us to specify a target.
+        .target = target,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "wnk",
+        .root_module = b.createModule(.{
+            // b.createModule defines a new module just like b.addModule but,
+            // unlike b.addModule, it does not expose the module to consumers of
+            // this package, which is why in this case we don't have to give it a name.
+            .root_source_file = b.path("src/main.zig"),
+            // Target and optimization levels must be explicitly wired in when
+            // defining an executable or library (in the root module), and you
+            // can also hardcode a specific target for an executable or library
+            // definition if desireable (e.g. firmware for embedded devices).
+            .target = target,
+            .optimize = optimize,
+            // List of modules available for import in source files part of the
+            // root module.
+            .imports = &.{
+                // Here "wnk" is the name you will use in your source code to
+                // import this module (e.g. `@import("wnk")`). The name is
+                // repeated because you are allowed to rename your imports, which
+                // can be extremely useful in case of collisions (which can happen
+                // importing modules from different packages).
+                .{ .name = "wnk", .module = mod },
+            },
+        }),
+    });
+
+    exe.root_module.addImport("dvui", dvui_dep.module("dvui_sdl3"));
+    // For windows
+    exe.linkLibC();
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    // By making the run step depend on the default step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // Creates an executable that will run `test` blocks from the provided module.
+    // Here `mod` needs to define a target, which is why earlier we made sure to
+    // set the releative field.
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
+    });
+
+    // A run step that will run the test executable.
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+
+    // Creates an executable that will run `test` blocks from the executable's
+    // root module. Note that test executables only test one module at a time,
+    // hence why we have to create two separate ones.
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+
+    // A run step that will run the second test executable.
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    // A top level step for running all tests. dependOn can be called multiple
+    // times and since the two run steps do not depend on one another, this will
+    // make the two of them run in parallel.
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
+    test_step.dependOn(&run_exe_tests.step);
+
+    // Just like flags, top level steps are also listed in the `--help` menu.
+    //
+    // The Zig build system is entirely implemented in userland, which means
+    // that it cannot hook into private compiler APIs. All compilation work
+    // orchestrated by the build system will result in other Zig compiler
+    // subcommands being invoked with the right flags defined. You can observe
+    // these invocations when one fails (or you pass a flag to increase
+    // verbosity) to validate assumptions and diagnose problems.
+    //
+    // Lastly, the Zig build system is relatively simple and self-contained,
+    // and reading its source code will allow you to master it.
+}
---
+++ src/main.zig
@@ -0,0 +1,175 @@
+const std = @import("std");
+const dvui = @import("dvui");
+
+// Search buffer for the input field
+var search_buffer: [256]u8 = undefined;
+var search_len: usize = 0;
+var search_initialized = false;
+
+// Mock search results
+const SearchResult = struct {
+    title: []const u8,
+    subtitle: []const u8,
+    icon: []const u8,
+};
+
+const mock_results = [_]SearchResult{
+    .{ .title = "Calculator", .subtitle = "Application", .icon = "=" },
+    .{ .title = "Calendar", .subtitle = "System Preferences", .icon = "C" },
+    .{ .title = "Camera", .subtitle = "Devices", .icon = "O" },
+    .{ .title = "Chrome", .subtitle = "Web Browser", .icon = "@" },
+    .{ .title = "Code", .subtitle = "Development", .icon = "#" },
+};
+
+// Declare as dvui App
+pub const dvui_app: dvui.App = .{
+    .config = .{
+        .options = .{
+            .size = .{ .w = 700.0, .h = 500.0 },
+            .min_size = .{ .w = 600.0, .h = 400.0 },
+            .title = "Wink Launcher",
+            .window_init_options = .{},
+        },
+    },
+    .frameFn = AppFrame,
+    .initFn = AppInit,
+    .deinitFn = AppDeinit,
+};
+
+// Use the main function provided by dvui.App
+pub const main = dvui.App.main;
+pub const panic = dvui.App.panic;
+pub const std_options: std.Options = .{
+    .logFn = dvui.App.logFn,
+};
+
+// Executed before the first frame, after backend and dvui.Window.init()
+pub fn AppInit(win: *dvui.Window) !void {
+    _ = win;
+    // Initialize search buffer
+    @memset(&search_buffer, 0);
+    search_initialized = true;
+}
+
+// Executed before application closes, before dvui.Window.deinit()
+pub fn AppDeinit() void {
+    // Clean up resources
+}
+
+// Executed every frame to draw UI
+pub fn AppFrame() !dvui.App.Result {
+    // Main container with padding
+    var main_box = dvui.box(@src(), .{ .dir = .vertical }, .{
+        .expand = .both,
+        .background = true,
+        .color_fill = .{ .r = 0x1e, .g = 0x1e, .b = 0x2e },
+    });
+    defer main_box.deinit();
+
+    // Top spacing
+    _ = dvui.spacer(@src(), .{ .min_size_content = .{ .h = 20 } });
+
+    // Search bar container
+    {
+        var search_box = dvui.box(@src(), .{ .dir = .horizontal }, .{
+            .expand = .horizontal,
+            .margin = .{ .x = 20, .y = 0, .w = 20, .h = 0 },
+        });
+        defer search_box.deinit();
+
+        // Search icon
+        dvui.label(@src(), "[>", .{}, .{ .font_style = .title });
+
+        _ = dvui.spacer(@src(), .{ .min_size_content = .{ .w = 10 } });
+
+        // Search input field
+        var te = dvui.textEntry(@src(), .{
+            .text = .{ .buffer = &search_buffer },
+            .placeholder = "Search for apps, files, and more...",
+        }, .{
+            .expand = .horizontal,
+            .font_style = .title_4,
+            .color_fill = .{ .r = 0x2a, .g = 0x2a, .b = 0x3e },
+            .color_text = .{ .r = 0xff, .g = 0xff, .b = 0xff },
+        });
+        search_len = te.len;
+        te.deinit();
+    }
+
+    _ = dvui.spacer(@src(), .{ .min_size_content = .{ .h = 20 } });
+
+    // Divider
+    _ = dvui.separator(@src(), .{ .expand = .horizontal, .margin = .{ .x = 20 } });
+
+    _ = dvui.spacer(@src(), .{ .min_size_content = .{ .h = 10 } });
+
+    // Results panel with scroll
+    {
+        var scroll = dvui.scrollArea(@src(), .{}, .{
+            .expand = .both,
+            .margin = .{ .x = 20, .y = 0, .w = 20, .h = 20 },
+        });
+        defer scroll.deinit();
+
+        // Filter and display results
+        for (mock_results, 0..) |result, i| {
+            // Simple filter based on search text
+            const should_show = if (search_len == 0)
+                true
+            else blk: {
+                const search_text = search_buffer[0..search_len];
+                // Simple contains check
+                break :blk std.mem.indexOf(u8, result.title, search_text) != null or
+                    std.mem.indexOf(u8, result.subtitle, search_text) != null;
+            };
+
+            if (should_show) {
+                // Result item
+                var item_box = dvui.box(@src(), .{ .dir = .horizontal }, .{
+                    .expand = .horizontal,
+                    .id_extra = i,
+                    .background = true,
+                    .border = .{},
+                    .corner_radius = .{ .x = 8, .y = 8, .w = 8, .h = 8 },
+                    .padding = .{ .x = 12, .y = 8, .w = 12, .h = 8 },
+                    .margin = .{ .x = 0, .y = 4, .w = 0, .h = 4 },
+                    .color_fill = .{ .r = 0x2a, .g = 0x2a, .b = 0x3e },
+                });
+                defer item_box.deinit();
+
+                // Icon
+                dvui.label(@src(), "{s}", .{result.icon}, .{
+                    .font_style = .title,
+                    .id_extra = i,
+                });
+
+                _ = dvui.spacer(@src(), .{ .min_size_content = .{ .w = 12 }, .id_extra = i });
+
+                // Text content
+                {
+                    var text_box = dvui.box(@src(), .{ .dir = .vertical }, .{
+                        .expand = .horizontal,
+                        .id_extra = i,
+                    });
+                    defer text_box.deinit();
+
+                    dvui.label(@src(), "{s}", .{result.title}, .{
+                        .font_style = .title_4,
+                        .color_text = .{ .r = 0xff, .g = 0xff, .b = 0xff },
+                        .id_extra = i,
+                    });
+
+                    _ = dvui.spacer(@src(), .{ .min_size_content = .{ .h = 2 }, .id_extra = i + 1000 });
+
+                    dvui.label(@src(), "{s}", .{result.subtitle}, .{
+                        .font_style = .caption,
+                        .color_text = .{ .r = 0x88, .g = 0x88, .b = 0x99 },
+                        .id_extra = i * 100,
+                    });
+                }
+            }
+        }
+    }
+
+    return .ok;
+}
